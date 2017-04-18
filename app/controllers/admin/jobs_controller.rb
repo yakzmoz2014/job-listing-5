99ac336a872f7b0c167ce86888e02e1929fa489c
wangdonghui <wangdonghuiibt@gmail.com>
2017-04-18 15:21:45 +0800

    admin can hide & show job

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,6 +1,7 @@
 class Admin::JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :require_is_admin
+  layout "admin"
 
   def index
     @jobs = Job.all
@@ -33,6 +34,17 @@ class Admin::JobsController < ApplicationController
     redirect_to jobs_path, alert: "Job was deleted"
   end
 
+  def publish
+    @job = Job.find(params[:id])
+    @job.publish!
+    redirect_to :back
+  end
+
+  def hide
+    @job = Job.find(params[:id])
+    @job.hide!
+    redirect_to :back
+  end
 
 
   private
