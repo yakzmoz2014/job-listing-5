b042c879e11a838d3aa12722fc7cce769999309f
wangdonghui <wangdonghuiibt@gmail.com>
2017-04-18 14:30:43 +0800

    add more column to job & admin can hide job

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -33,11 +33,11 @@ class Admin::JobsController < ApplicationController
     redirect_to jobs_path, alert: "Job was deleted"
   end
 
-  
+
 
   private
 
   def job_params
-    params.require(:job).permit(:title, :description)
+    params.require(:job).permit(:title, :description, :wage_lower_bound, :wage_upper_bound, :contact_email, :is_hidden)
   end
 end
